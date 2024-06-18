ab94b2a581093899aa21e241777a03645dd264fb
Xander <xanderheilman@gmail.com>
2024-06-17 21:16:04 -0400

    Trying to fix express

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 //app.use(express.json()); //allows app.post route handler to parse json
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
