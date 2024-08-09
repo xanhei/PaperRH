0d3f974c06fefcd4e812304c1511ea6b77837567
Xander <xanderheilman@gmail.com>
2024-08-09 02:18:01 -0400

    Trying to fix cors issue

--- server/server.js
+++ server/server.js
@@ -11,6 +11,11 @@ const corsOps = {
   optionsSuccessStatus: 200
 };
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 //initialize db connection
 const {MongoClient, ServerApiVersion} = require("mongodb");
