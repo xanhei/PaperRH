21a86cca18beb5bfd6173f3afbee73f7286c1f82
Xander <xanderheilman@gmail.com>
2024-08-21 22:08:03 -0400

    Adding load message when sign in is successful

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -6,11 +6,16 @@ const phrases = [["Already have an account? ", "Sign In"], ["Don't have an accou
 const LoginScreen = (props) => {
   const [phraseIndex, setPhraseIndex] = useState(1);
   const [alert, setAlert] = useState("");
+  const [boxColor, setBoxColor] = useState("");
 
   const loginCheck = async () => {
+    setAlert("Loading...");
+    setBoxColor("");
     const res = await props.login(document.querySelector("#u").value, document.querySelector("#p").value, phraseIndex === 0);
-    if(!res !== "")
-      setAlert(res);
+    if(res !== "") {
+      setAlert("Error: " + res);
+      setBoxColor("red");
+    }
   }
   const enterCheck = (event) => {
     if(event.key === "Enter")
@@ -26,7 +31,7 @@ const LoginScreen = (props) => {
       </div>
       <p className="disclaimer">Username and password do not have to be complicated</p>
       <div className="userPass">
-        {alert !== "" ? <p className="loginError">Error: {alert}</p> : <></>}
+        {alert !== "" ? <p className="loginError" style={{borderColor: boxColor}}>{alert}</p> : <></>}
         <input id="u" className="userPassInput" placeholder="Username" autoComplete="off" onKeyDown={(event) => enterCheck(event)}></input>
         <input id="p" className="userPassInput" type="password" placeholder="Password" onKeyDown={(event) => enterCheck(event)}></input>
         <button style={{cursor: "pointer"}} onClick={() => loginCheck()}>{phrases[(phraseIndex + 1) % 2][1]}</button>
